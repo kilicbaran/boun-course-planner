0bb0d3faf6721a84ac222879437c8edcdc7965a5
Baran Kılıç <baran.kilic@boun.edu.tr>
2022-02-05 14:38:18 +0300

    fix: google analytics

--- src/lib/GoogleAnalytics.svelte
+++ src/lib/GoogleAnalytics.svelte
@@ -1,14 +1,17 @@
-<script context="module">
-    const GTAG_ID = import.meta.env.VITE_GTAG_ID;
-    const scriptPart = `<script>window.dataLayer = window.dataLayer || [];function gtag() {dataLayer.push(arguments);}gtag("js", new Date());gtag("config", ${GTAG_ID});<\/script>`;
-</script>
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <svelte:head>
     {#if import.meta.env.PROD}
         <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id={GTAG_ID}"></script>
-        {@html scriptPart}
+            src="https://www.googletagmanager.com/gtag/js?id=G-6YH5E6DSP4"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+
+            gtag("config", "G-6YH5E6DSP4");
+        </script>
     {/if}
 </svelte:head>
